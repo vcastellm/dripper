b0fbe03f8ea570b3abad8162ddd7c0303a0ed47d
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-04-22 00:51:15 +0200

    We are now able to install dripper in a custom path

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 set -e
 
 if [ $(id -u) = "0" ]; then
-  DRIPPER_ROOT=/usr/local
+  DEFAULT_DRIPPER_ROOT=/usr/local
 else
-  DRIPPER_ROOT=$HOME/.packages
+  DEFAULT_DRIPPER_ROOT=$HOME/.packages
+fi
+
+if [ ! -n "${DRIPPER_ROOT}" ]; then
+  DRIPPER_ROOT=$DEFAULT_DRIPPER_ROOT
 fi
 
 BIN_PATH="$DRIPPER_ROOT/bin"
@@ -30,9 +34,14 @@ done
 
 echo "Installed dripper at $DRIPPER_ROOT"
 
+# Set a default user path so we show the user some instructions in
+# case he didn't install Dripper in the default location.
+PATH="$DEFAULT_DRIPPER_ROOT:/usr/bin:/bin"
+
 if [[ ! $(which drip) ]]; then
   echo "
-Seems you still have not added '.packages' to the load path:
+Seems you still have not added DRIPPER_ROOT to the load path. Please
+update your \`.bashrc' with the following settings:
 
   DRIPPER_ROOT=$DRIPPER_ROOT
   if [ -d \$DRIPPER_ROOT ]; then
